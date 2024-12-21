94f432363a24c370b69d6226614b74ab442f6393
somenoe <otashi24@gmail.com>
2024-12-21 17:19:32 +0700

    🔨 Add hash value to in saved state

--- watch.bat
+++ watch.bat
@@ -6,9 +6,15 @@ setlocal enabledelayedexpansion
 set "src=src"
 set "lastState="
 
-REM Get initial state
+REM Get initial state with content hash
 for /f "delims=" %%F in ('dir /s /b /a-d "%src%\*.*"') do (
-	set "lastState=!lastState!%%F %%~zF %%~tF$"
+    set "hash="
+    for /f "skip=1 tokens=* delims=" %%H in ('certutil -hashfile "%%F" MD5 2^>nul') do (
+        if not defined hash (
+            set "hash=%%H"
+            set "lastState=!lastState!%%F:!hash!$"
+        )
+    )
 )
 
 echo Watching for changes in %src%...
@@ -20,17 +26,22 @@ timeout /t 1 /nobreak > nul
 REM Clear current state
 set "currentState="
 
-REM Capture current state of files
+REM Capture current state with content hash
 for /f "delims=" %%F in ('dir /s /b /a-d "%src%\*.*"') do (
-	set "currentState=!currentState!%%F %%~zF %%~tF$"
+    set "hash="
+    for /f "skip=1 tokens=* delims=" %%H in ('certutil -hashfile "%%F" MD5 2^>nul') do (
+        if not defined hash (
+            set "hash=%%H"
+            set "currentState=!currentState!%%F:!hash!$"
+        )
+    )
 )
 
 REM Compare states
 if not "!lastState!"=="!currentState!" (
-	echo %lastState%
-	echo [%TIME%] Change detected! Recompiling...
-	call update.bat
-	set "lastState=!currentState!"
+    echo [%TIME%] Change detected! Recompiling...
+    call update.bat
+    set "lastState=!currentState!"
 )
 
 goto loop
